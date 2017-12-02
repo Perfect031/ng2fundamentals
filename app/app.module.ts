2fc7d506f27971038ed5b84302bf74b320596969
Gujarathi <nipun.gujarathi@capgemini.com>
2017-12-02 15:05:46 +0530

    New Component Added

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app-component';
+import { ProductComponent } from './products/products-app-component';
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [EventsAppComponent],
+    declarations: [EventsAppComponent, ProductComponent],
     bootstrap: [EventsAppComponent]
 })
+
 
 export class AppModule{
 
--- app/events-app-component.ts
+++ app/events-app-component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'events-app',
-    template: '<h2> Hello, {{name}}</h2>'
+    template: '<h2> Hello, {{name}}</h2><div><my-products></my-products></div>'
 })
 
 export  class EventsAppComponent{
